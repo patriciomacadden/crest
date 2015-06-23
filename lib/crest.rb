96ca3fff3fc8fc18012baca6f02b2c0305d70647
Patricio Mac Adden <patriciomacadden@gmail.com>
2015-06-23 18:43:30 -0300

    add a find_* for collections (used in list_*)

--- lib/crest.rb
+++ lib/crest.rb
@@ -2,87 +2,101 @@ require 'inflecto'
 
 module Crest
   def rest(klass, options = {}, &block)
-    self.class.send :define_method, :"list_#{Inflecto.underscore Inflecto.pluralize(klass)}" do
-      render "#{Inflecto.underscore Inflecto.pluralize(klass)}/list", :"#{Inflecto.underscore Inflecto.pluralize(klass)}" => klass.all
-    end unless respond_to? :"list_#{Inflecto.underscore Inflecto.pluralize(klass)}"
+    self.class.send :define_method, :"find_#{object_name klass}" do |id|
+      klass[id]
+    end unless respond_to? :"find_#{object_name klass}"
 
-    self.class.send :define_method, :"create_#{Inflecto.underscore klass}" do |object_params|
+    self.class.send :define_method, :"find_#{collection_name klass}" do
+      klass.all
+    end unless respond_to? :"find_#{collection_name klass}"
+
+    self.class.send :define_method, :"list_#{collection_name klass}" do
+      render "#{collection_name klass}/list", :"#{collection_name klass}" => send(:"find_#{collection_name klass}")
+    end unless respond_to? :"list_#{collection_name klass}"
+
+    self.class.send :define_method, :"create_#{object_name klass}" do |object_params|
       object = klass.new object_params
       if object.valid?
         object.save
-        res.redirect "#{options[:base_uri]}/#{Inflecto.underscore Inflecto.pluralize(klass)}/#{object.id}"
+        res.redirect "#{options[:base_uri]}/#{collection_name klass}/#{object.id}"
       else
-        render "#{Inflecto.underscore Inflecto.pluralize(klass)}/new", :"#{Inflecto.underscore klass}" => object
+        render "#{collection_name klass}/new", :"#{object_name klass}" => object
       end
-    end unless respond_to? :"create_#{Inflecto.underscore klass}"
+    end unless respond_to? :"create_#{object_name klass}"
 
-    self.class.send :define_method, :"new_#{Inflecto.underscore klass}" do
-      render "#{Inflecto.underscore Inflecto.pluralize(klass)}/new", :"#{Inflecto.underscore klass}" => klass.new
-    end unless respond_to? :"new_#{Inflecto.underscore klass}"
+    self.class.send :define_method, :"new_#{object_name klass}" do
+      render "#{collection_name klass}/new", :"#{object_name klass}" => klass.new
+    end unless respond_to? :"new_#{object_name klass}"
 
-    self.class.send :define_method, :"show_#{Inflecto.underscore klass}" do |object|
-      render "#{Inflecto.underscore Inflecto.pluralize(klass)}/show", :"#{Inflecto.underscore klass}" => object
-    end unless respond_to? :"show_#{Inflecto.underscore klass}"
+    self.class.send :define_method, :"show_#{object_name klass}" do |object|
+      render "#{collection_name klass}/show", :"#{object_name klass}" => object
+    end unless respond_to? :"show_#{object_name klass}"
 
-    self.class.send :define_method, :"update_#{Inflecto.underscore klass}" do |object, object_params|
+    self.class.send :define_method, :"update_#{object_name klass}" do |object, object_params|
       object.set_all object_params
       if object.valid?
         object.save
-        res.redirect "#{options[:base_uri]}/#{Inflecto.underscore Inflecto.pluralize(klass)}/#{object.id}"
+        res.redirect "#{options[:base_uri]}/#{collection_name klass}/#{object.id}"
       else
-        render "#{Inflecto.underscore Inflecto.pluralize(klass)}/edit", :"#{Inflecto.underscore klass}" => object
+        render "#{collection_name klass}/edit", :"#{object_name klass}" => object
       end
-    end unless respond_to? :"update_#{Inflecto.underscore klass}"
+    end unless respond_to? :"update_#{object_name klass}"
 
-    self.class.send :define_method, :"delete_#{Inflecto.underscore klass}" do |object|
+    self.class.send :define_method, :"delete_#{object_name klass}" do |object|
       object.delete
-      res.redirect "#{options[:base_uri]}/#{Inflecto.underscore Inflecto.pluralize(klass)}"
-    end unless respond_to? :"delete_#{Inflecto.underscore klass}"
+      res.redirect "#{options[:base_uri]}/#{collection_name klass}"
+    end unless respond_to? :"delete_#{object_name klass}"
 
-    self.class.send :define_method, :"edit_#{Inflecto.underscore klass}" do |object|
-      render "#{Inflecto.underscore Inflecto.pluralize(klass)}/edit", :"#{Inflecto.underscore klass}" => object
-    end unless respond_to? :"edit_#{Inflecto.underscore klass}"
-
-    self.class.send :define_method, :"find_#{Inflecto.underscore klass}" do |id|
-      klass[id]
-    end unless respond_to? :"find_#{Inflecto.underscore klass}"
+    self.class.send :define_method, :"edit_#{object_name klass}" do |object|
+      render "#{collection_name klass}/edit", :"#{object_name klass}" => object
+    end unless respond_to? :"edit_#{object_name klass}"
 
     block.call unless block.nil?
 
     on root do
       on get do
-        send :"list_#{Inflecto.underscore Inflecto.pluralize(klass)}"
+        send :"list_#{collection_name klass}"
       end
 
-      on post, param(:"#{Inflecto.underscore klass}") do |object_params|
-        send :"create_#{Inflecto.underscore klass}", object_params
+      on post, param(:"#{object_name klass}") do |object_params|
+        send :"create_#{object_name klass}", object_params
       end
     end
 
     on get, 'new' do
-      send :"new_#{Inflecto.underscore klass}"
+      send :"new_#{object_name klass}"
     end
 
     on :id do |id|
-      object = send :"find_#{Inflecto.underscore klass}", id
+      object = send :"find_#{object_name klass}", id
 
       on root do
         on get do
-          send :"show_#{Inflecto.underscore klass}", object
+          send :"show_#{object_name klass}", object
         end
 
-        on put, param(:"#{Inflecto.underscore klass}") do |object_params|
-          send :"update_#{Inflecto.underscore klass}", object, object_params
+        on put, param(:"#{object_name klass}") do |object_params|
+          send :"update_#{object_name klass}", object, object_params
         end
 
         on delete do
-          send :"delete_#{Inflecto.underscore klass}", object
+          send :"delete_#{object_name klass}", object
         end
       end
 
       on 'edit' do
-        send :"edit_#{Inflecto.underscore klass}", object
+        send :"edit_#{object_name klass}", object
       end
     end
+  end
+
+  private
+
+  def collection_name(klass)
+    Inflecto.underscore Inflecto.pluralize(klass)
+  end
+
+  def object_name(klass)
+    Inflecto.underscore klass
   end
 end
